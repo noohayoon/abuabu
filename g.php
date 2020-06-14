7c4aa24b876e49c6b4079794986030b4b56a82e4
noohayoon <36268736+noohayoon@users.noreply.github.com>
2020-06-15 02:01:25 +0700

    Update and rename jf2x.php to g.php

--- g.php
+++ g.php
@@ -55,7 +55,7 @@ echo color("white","                  Format Kode 62*** \n");
         echo color("white",".");
         sleep(5);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"EATLAH"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"ALFAMARTJUNI"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","Message: ".$message);
